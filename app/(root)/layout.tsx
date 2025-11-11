b4e1d512adee78ab86103dec95d5dda8a2b23693
Phong Nguyen <skyfalljoss@gmail.com>
2025-11-10 20:47:14 -0500

    02: build UI for home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React, { ReactNode } from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children} </div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex item-center gap-2'>
+          <Image
+            src="/logo"
+            alt= "PrepInterview"
+            width={38}
+            height={32}
+          />
+          <h2 className='text-primary-100'>PrepInterview</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 
