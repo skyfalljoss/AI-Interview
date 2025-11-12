d2ef8a5479b253143325230e795c45de2d128ff4
Phong Nguyen <skyfalljoss@gmail.com>
2025-11-11 21:58:23 -0500

    feat: authentication

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,12 +1,33 @@
 'use server';
-import {db} from "@/firebase/admin"
+import {auth, db} from "@/firebase/admin"
+import { cookies } from "next/headers";
 
-export async function signUp(params: SignInParams){
+
+const ONE_WEEK = 60 * 60 * 24 * 7 * 1000 // 1 week
+
+export async function signUp(params: SignUpParams){
     const {uid, name, email } = params;
 
     try{
-        const userRecord = await db.collection('user').doc(uid).get();
-    }catch(error:any){
+        const userRecord = await db.collection('users').doc(uid).get();
+
+        if (userRecord.exists) {
+            return{
+                success:false,
+                message:"User already exists. Please sign in instead"
+            }
+        }
+
+        await db.collection('users').doc(uid).set({
+            name, email
+        })
+
+        return{
+            success: true,
+            message: "Account created successfully. Please sign in."
+        }
+
+    }catch(error : any){
         console.error('Error creating a user', error);
 
         if(error.code === 'auth/email-already-exists'){
@@ -22,4 +43,89 @@ export async function signUp(params: SignInParams){
         }
 
     }
+}
+
+export const signIn = async (params: SignInParams) => {
+    const {email, idToken} = params
+
+    try{
+        const userRecord = await auth.getUserByEmail(email);
+
+        if(!userRecord){
+            return {
+                success:false,
+                message: "User doesn't exist. Create a new account."
+            }
+        }
+
+        await setSessionCookie(idToken);
+        return {
+            success: true,
+            message: 'Sign in succesful.'
+        }
+
+    }catch (error){
+        console.log(error)
+
+        return {
+            success:false,
+            message: "Failed to log in"
+        }
+    }
+}
+
+export const setSessionCookie = async (idToken: string) =>{
+    const cookieStore = await cookies();
+
+    const sessionCookie = await auth.createSessionCookie(idToken, {
+        expiresIn: ONE_WEEK,
+
+    })
+
+    cookieStore.set('session', sessionCookie, {
+        maxAge: ONE_WEEK,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+
+    })
+
+}
+
+export async function getCurrentUser(): Promise <User | null> {
+    const cookieStore = await cookies();
+
+    const sessionCookie = cookieStore.get('session')?.value;
+
+    if(!sessionCookie) {
+        console.log("No session cookie found");
+        return null;
+    }
+
+    try{
+        const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+
+        const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+
+        if(!userRecord.exists) {
+            console.log("User document doesn't exist in Firestore");
+            return null;
+        }
+        
+        return {
+            ...userRecord.data(),
+            id:userRecord.id
+        } as User;
+
+    }catch(error) {
+        console.log(error)
+        return null;
+    }
+}
+
+export async function isAuthenticated(){
+
+    const user = await getCurrentUser();
+
+    return !!user;
 }
